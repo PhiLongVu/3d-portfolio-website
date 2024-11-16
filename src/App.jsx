61fd13f72103693c418012b13291bb1bb6981109
PhiLongVu <148706236+PhiLongVu@users.noreply.github.com>
2024-11-16 23:41:50 +1100

    Added 3D scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import NavBar from './components/navbar';
+import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main className="bg-slate-300/20">
+      <Router>
+        <NavBar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
+
+const Home = () => {
+    return (
+        <section className='w-full h-screen relative'>
+            <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+                Popup
+            </div>
+            <Canvas className='w-full h-screen bg-transparent'
+            camera={{near: 0.1, far: 1000}}>
+                <Suspense fallback={<Loader />}> 
+                <directionalLight></directionalLight>
+                <ambientLight></ambientLight> 
+                <spotLight></spotLight>
+                <hemisphereLight></hemisphereLight>
+                <Island />
+                </Suspense>
+            </Canvas>
+        </section>
+    )
+}
+
+export default Home;
