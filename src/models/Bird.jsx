67a5716db81c0038861b4773463715cb267bd92d
PhiLongVu <148706236+PhiLongVu@users.noreply.github.com>
2024-11-18 00:45:46 +1100

    added styling for home page elements

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -10,30 +10,6 @@ const Bird = () => {
     useEffect(() => {
         actions["Take 001"].play();
     }, [])
-    // useFrame(({ clock, camera }) => {
-    //     // Update the Y position to simulate bird-like motion using a sine wave
-    //     ref.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
-    
-    //     // Check if the bird reached a certain endpoint relative to the camera
-    //     if (ref.current.position.x > camera.position.x + 10) {
-    //       // Change direction to backward and rotate the bird 180 degrees on the y-axis
-    //       ref.current.rotation.y = Math.PI;
-    //     } else if (ref.current.position.x < camera.position.x - 10) {
-    //       // Change direction to forward and reset the bird's rotation
-    //       ref.current.rotation.y = 0;
-    //     }
-    
-    //     // Update the X and Z positions based on the direction
-    //     if (ref.current.rotation.y === 0) {
-    //       // Moving forward
-    //       ref.current.position.x += 0.01;
-    //       ref.current.position.z -= 0.01;
-    //     } else {
-    //       // Moving backward
-    //       ref.current.position.x -= 0.01;
-    //       ref.current.position.z += 0.01;
-    //     }
-    //   });
     useFrame(({clock, camera}) => {
         ref.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
         if (ref.current.position.x > camera.position.x + 10) {
