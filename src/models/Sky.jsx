7146a860ca0b4e4d1c9f978b1f70b350f318b550
PhiLongVu <148706236+PhiLongVu@users.noreply.github.com>
2024-11-17 21:54:28 +1100

    Added extra features like Animations and Interactions for the island and the bird

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import React, { useRef } from 'react'
+import skyScene from '../assets/3ds/sky.glb'
+import { useFrame } from '@react-three/fiber'
 
-const Sky = () => {
+const Sky = ({isRotating}) => {
+    const sky = useGLTF(skyScene)
+    const ref = useRef()
+    useFrame((_, delta) => {
+        if (isRotating) {
+            ref.current.rotation.y += 0.5 * delta
+        }
+    }
+    )
   return (
-    <mesh></mesh>
+    <mesh ref={ref}>
+        <primitive object={sky.scene} />
+    </mesh>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 const Home = () => {
+    const [isRotating, setIsRotating] = useState(false);
+    const [currentStage, setCurrentStage] = useState(1);
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
@@ -15,27 +19,52 @@ const Home = () => {
         } else {
             screenScale = [1, 1, 1];
         }
-        return { screenScale, screenPosition, screenRotation };
+        return [screenScale, screenPosition, screenRotation];
     }
-    const { screenScale, screenPosition, screenRotation } = adjustIslandForScreenSize();
+    const adjustPlaneForScreenSize = () => {
+        let planeScreenScale, planeScreenPosition;
+        if (window.innerWidth < 768) {
+            planeScreenScale = [1.5, 1.5, 1.5];
+            planeScreenPosition = [0, -1.5, 0];
+        } else {
+            planeScreenScale = [3, 3, 3];
+            planeScreenPosition = [0, 0, 0];
+        }
+        return [planeScreenScale, planeScreenPosition];
+    }
+    const [screenScale, screenPosition, screenRotation] = adjustIslandForScreenSize();
+    const [planeScreenScale, planeScreenPosition] = adjustPlaneForScreenSize();
+    console.log(planeScreenPosition, planeScreenScale);
     return (
         <section className='w-full h-screen relative'>
             <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
                 Popup
             </div>
-            <Canvas className='w-full h-screen bg-transparent'
-            camera={{near: 0.1, far: 1000}}>
-                <Suspense fallback={<Loader />}> 
-                <directionalLight position={[1,1,1]} intensity={2}></directionalLight>
-                <ambientLight intensity={0.5}></ambientLight> 
-                <spotLight></spotLight>
-                <hemisphereLight skyColor="#00fffb" groundColor="#5e005d" intensity={1}></hemisphereLight>
-                <Sky />
-                <Island 
-                    position={screenPosition}
-                    scale={screenScale}
-                    rotation={screenRotation}
-                />
+            <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+                camera={{ near: 0.1, far: 1000 }}>
+                <Suspense fallback={<Loader />}>
+                    <directionalLight position={[1, 1, 1]} intensity={2}></directionalLight>
+                    <ambientLight intensity={0.5}></ambientLight>
+                    <spotLight></spotLight>
+                    <hemisphereLight skyColor="#00fffb" groundColor="#5e005d" intensity={1}></hemisphereLight>
+                    <Bird />
+                    <Sky 
+                    isRotating={isRotating}
+                    />
+                    <Island
+                        position={screenPosition}
+                        scale={screenScale}
+                        rotation={screenRotation}
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
+                        setCurrentStage={setCurrentStage}
+                    />
+                    <Plane
+                        isRotating={isRotating}
+                        planePosition={planeScreenPosition}
+                        planeScale={planeScreenScale}
+                        rotation={[0, 20, 0]}
+                    />
                 </Suspense>
             </Canvas>
         </section>
