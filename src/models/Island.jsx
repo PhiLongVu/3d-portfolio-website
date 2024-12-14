0aef672270ed60372a58100a52be137be1a31a8a
PhiLongVu <148706236+PhiLongVu@users.noreply.github.com>
2024-12-14 17:13:29 +1100

    added some visuals for project page

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -64,11 +64,13 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
                 setIsRotating(true)
             }
             islandRef.current.rotation.y += Math.PI * 0.01
+            rotationSpeed.current = 0.0125
         } else if (e.key === 'ArrowRight') {
             if(!isRotating) {
                 setIsRotating(true)
             }
             islandRef.current.rotation.y -= Math.PI * 0.01
+            rotationSpeed.current = -0.0125
         }
     }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Canvas } from "@react-three/fiber";
 import { useState, Suspense } from 'react'
 import Loader from '../components/Loader'
@@ -7,10 +7,24 @@ import HomeInfo from '../components/HomeInfo';
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
+import sakura from '../assets/sakura.mp3'
+import { soundoff, soundon } from '../assets/icons';
 
 const Home = () => {
+    const audioRef = useRef(new Audio(sakura));
+    audioRef.current.volume = 0.4;
+    audioRef.current.loop = true;
     const [isRotating, setIsRotating] = useState(false);
     const [currentStage, setCurrentStage] = useState(1);
+    const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+    useEffect(() => {
+        if (isPlayingMusic) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isPlayingMusic])
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
@@ -29,7 +43,7 @@ const Home = () => {
             planeScreenPosition = [0, -1.5, 0];
         } else {
             planeScreenScale = [3, 3, 3];
-            planeScreenPosition = [0, 0, 0];
+            planeScreenPosition = [0, -4, -4];
         }
         return [planeScreenScale, planeScreenPosition];
     }
@@ -39,7 +53,7 @@ const Home = () => {
     return (
         <section className='w-full h-screen relative'>
             <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-                {currentStage && <HomeInfo currentStage={currentStage}/>}
+                {currentStage && <HomeInfo currentStage={currentStage} />}
             </div>
             <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}>
@@ -49,8 +63,8 @@ const Home = () => {
                     <spotLight></spotLight>
                     <hemisphereLight skyColor="#00fffb" groundColor="#5e005d" intensity={1}></hemisphereLight>
                     <Bird />
-                    <Sky 
-                    isRotating={isRotating}
+                    <Sky
+                        isRotating={isRotating}
                     />
                     <Island
                         position={screenPosition}
@@ -62,12 +76,21 @@ const Home = () => {
                     />
                     <Plane
                         isRotating={isRotating}
-                        planePosition={planeScreenPosition}
-                        planeScale={planeScreenScale}
+                        position={planeScreenPosition}
+                        scale={planeScreenScale}
                         rotation={[0, 20, 0]}
                     />
                 </Suspense>
             </Canvas>
+
+            <div className='absolute bottom-2 left-2'>
+                <img
+                    src={!isPlayingMusic ? soundoff : soundon}
+                    alt='jukebox'
+                    onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+                    className='w-10 h-10 cursor-pointer object-contain'
+                />
+            </div>
         </section>
     )
 }
